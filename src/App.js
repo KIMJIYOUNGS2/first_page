6cecd889ca1e96d8ba8403d6f8bdd36d403d122e
inseop <inseop0813@gmail.com>
2022-12-22 15:40:28 +0900

    4일차 - Users finish

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Count from "./Count";
 import Effect from "./Effect";
 import Todo from "./Todo";
 import Button from "./Button";
+import BookList from "./BookList";
+import Users from "./Users";
 
 // 최상위 컴포넌트
 function App() {
@@ -10,10 +12,12 @@ function App() {
     <div>
       {/* <Count /> */}
       {/* <Effect /> */}
-      <Todo />
       {/* <Button text="첫 번째 버튼" work="" />
       <Button text="두 번째 버튼" work="" />
-      <Button text="세 번째 버튼" work="true" /> */}
+    <Button text="세 번째 버튼" work="true" /> */}
+      {/* <Todo /> */}
+      {/* <BookList /> */}
+      <Users />
     </div>
   );
 }
